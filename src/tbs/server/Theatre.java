493e1e9bbb8afdbe283ff4c37e7c3e45a8132325
Wendy <wryao64@gmail.com>
2018-03-28 13:32:35 +1300

    theatre class

---
+++ src/tbs/server/Theatre.java
@@ -0,0 +1,13 @@
+package tbs.server;
+
+public class Theatre {
+	private String _theatreID;
+	private int _theatreDim;
+	private int _floorArea;
+
+	public Theatre(String theatreID, int theatreDim, int floorArea) {
+		_theatreID = theatreID;
+		_theatreDim = theatreDim;
+		_floorArea = floorArea;
+	}
+}
